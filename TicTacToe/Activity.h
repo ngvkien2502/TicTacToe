6972acc63420bb0d85c669de3126bc21c87727a4
Nguyen Kien <110652995+ngvkien2502@users.noreply.github.com>
2023-12-23 11:14:26 +0700

    Code

---
+++ TicTacToe/Activity.h
@@ -0,0 +1,15 @@
+//#pragma once
+//#include <iostream>
+//#include "SFML\Graphics.hpp"
+//
+//using namespace std;
+//using namespace sf;
+//
+//class Activity
+//{
+//public:
+//	RenderWindow uWin;
+//	Activity(int w, int h);
+//	~Activity();
+//	void Display();
+//};
